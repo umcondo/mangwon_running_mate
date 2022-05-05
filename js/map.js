52bb3f4dd9b8a43932972c5ba447422d1f5946a9
umcondo <superpowerkdg@gmail.com>
2022-05-05 18:18:04 +0900

    현재 위치 버튼 변경

--- js/map.js
+++ js/map.js
@@ -16,19 +16,6 @@ function runningCourse(coordinates, MapCenter) {
 
   map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-  /* 컨트롤 추가 */
-  // // 지도 타입 변경 컨트롤을 생성한다
-  // var mapTypeControl = new kakao.maps.MapTypeControl();
-
-  // // 지도의 상단 우측에 지도 타입 변경 컨트롤을 추가한다
-  // map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-  // // 지도에 확대 축소 컨트롤을 생성한다
-  // var zoomControl = new kakao.maps.ZoomControl();
-
-  // // 지도의 우측에 확대 축소 컨트롤을 추가한다
-  // map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
   /* 러닝 코스 점찍고, 선연결 */
   var linePath = []; // 러닝 코스
 
@@ -122,6 +109,7 @@ function runningCourse(coordinates, MapCenter) {
   currentLocationBtn += `</button>`;
 
   let currentLocationContainer = document.createElement("div");
+  currentLocationContainer.classList = "currentLocationContainer";
   currentLocationContainer.innerHTML = currentLocationBtn;
 
   mapContainer.appendChild(currentLocationContainer);
