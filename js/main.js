e878b62e32a1a64e0db6a1203a8d727f1e6030ef
umcondo <superpowerkdg@gmail.com>
2022-05-12 16:44:13 +0900

    링크공유 현재링크복사버튼 구현

--- js/main.js
+++ js/main.js
@@ -215,3 +215,19 @@ function modalBtn() {
     }
   };
 }
+
+/*공유하기 - 현재 링크 복사 */
+
+function clip() {
+  let url = "";
+  const textarea = document.createElement("textarea");
+
+  document.body.appendChild(textarea);
+  url = window.document.location.href;
+  textarea.value = url;
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+
+  alert("복사되었습니다 !");
+}
